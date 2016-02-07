90802f51561e0adccc7621000514ddf02b929a82
Hugo Wood <hx4.5@free.fr>
2016-02-07 17:52:24 +0100

    chore: added git sha1 to built zip files

--- zip.js
+++ zip.js
@@ -5,12 +5,14 @@ const fs = require("fs")
 const archiver = require("archiver")
 const glob = require("glob")
 const path = require("path")
+const exec = require("child_process").execSync
 
-const buildDate = new Date().toISOString().replace(/:/g, "-")
-const buildDest = `./.builds/submission-sources-${buildDate}.zip`
+const sha1 = exec("git rev-parse HEAD", {encoding: "utf8"}).trim()
+const date = new Date().toISOString().replace(/:/g, "-")
+const dest = `./.builds/submission-sources-${date}-${sha1}.zip`
 
 try {
-  fs.mkdirSync(path.dirname(buildDest))
+  fs.mkdirSync(path.dirname(dest))
 } catch (err) {
   if (err.code !== "EEXIST") throw err
 }
@@ -19,4 +21,4 @@ _.tap(archiver('zip', {}), function (archive) {
   _.each(glob.sync("!(node_modules)", {}), function (file) {
     archive.file(file, {name: path.basename(file)})
   })
-}).finalize().pipe(fs.createWriteStream(buildDest))
+}).finalize().pipe(fs.createWriteStream(dest))
