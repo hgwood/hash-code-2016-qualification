8c799caaca63509752b7fc3e7c2fc4cb3ccc3e0c
Hugo Wood <hx4.5@free.fr>
2016-02-11 20:04:58 +0100

    feat: all input files at once

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 process.env.DEBUG = "*"
 
-const problem = require("./read")("./mother_of_all_warehouses.in.txt")
-const solution = require("./solver")(problem)
-require("./write")("./mother_of_all_warehouses.out.txt", solution)
+const _ = require("lodash")
+
+_.each(["mother_of_all_warehouses", "redundancy", "busy_day"], function (file) {
+  const problem = require("./read")(`./${file}.in.txt`)
+  const solution = require("./solver")(problem)
+  require("./write")(`./${file}.out.txt`, solution)
+})
