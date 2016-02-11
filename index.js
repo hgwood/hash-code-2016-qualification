2df407bc663d738ded002093d501f550385befb8
Hugo Wood <hx4.5@free.fr>
2016-02-11 20:35:07 +0100

    feat: files to process can be given as cmd line params

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ process.env.DEBUG = "*"
 
 const _ = require("lodash")
 
-_.each(["mother_of_all_warehouses", "redundancy", "busy_day"], function (file) {
-  const problem = require("./read")(`./${file}.in.txt`)
+let files = _.slice(process.argv, 2)
+files = _.isEmpty(files) ? ["./mother_of_all_warehouses.in.txt", "./redundancy.in.txt", "./busy_day.in.txt"] : files
+
+_.each(files, function (file) {
+  const problem = require("./read")(file)
   const solution = require("./solver")(problem)
-  require("./write")(`./${file}.out.txt`, solution)
+  require("./write")(`${file}.out.txt`, solution)
 })
