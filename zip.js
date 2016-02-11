7f6cfe80dc8f2cf7aeefdd31fd45d7dc049bb89e
Julien Landuré <julien.landure@gmail.com>
2016-02-11 18:30:26 +0100

    tayo!

--- zip.js
+++ zip.js
@@ -26,7 +26,9 @@ const archive = archiver('zip')
 _.each(files, file => archive.file(file, {name: path.basename(file)}))
 archive.finalize().pipe(fs.createWriteStream(dest))
 
+console.log(process.argv);
 if(process.argv[3] === "upload") {
+
   upload("out.txt", dest);
 }
 console.log(`wrote ${files.length} files to ${dest}`)
