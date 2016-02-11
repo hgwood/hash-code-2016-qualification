af2b4aa24f0c7a1ae4f4fe089d968540b44756e5
Julien Landuré <julien.landure@gmail.com>
2016-02-11 20:32:13 +0100

    stop drones with distance

--- solver.js
+++ solver.js
@@ -5,14 +5,22 @@ const _ = require("lodash")
 
 module.exports = function solve(problem) {
   const commands = []
+
+  const drones = new Array(problem.header.ndrones).fill(problem.header.nturns);
   debug(problem.header)
   _.each(problem.orders, function (order, iorder) {
     const drone = iorder % problem.header.ndrones
+
     _.each(order.types, function (type) {
       const warehouse = findWarehouseForProduct(problem, type)
+      const distanceToRun = distance(problem.warehouses[warehouse], order) + 1000;
+      if(drones[drone] < distanceToRun) return commands;
+
       commands.push({command: "load", drone, warehouse, type, quantity: 1})
       problem.warehouses[warehouse].products[type] -= 1
       commands.push({command: "deliver", drone, order: iorder, type, quantity: 1})
+      drones[drone] -= distanceToRun + 1
+      console.log(drone, "->", drones[drone]);
     })
   })
   return commands
